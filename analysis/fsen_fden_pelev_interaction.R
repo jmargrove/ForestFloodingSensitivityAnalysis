819305c3c1294c76ab6f2955cb467b251d6306cc
t03jam8 <james.margrove@usys.ethz.ch>
2018-02-07 19:15:28 +0100

    feat: interaction plot

--- analysis/fsen_fden_pelev_interaction.R
+++ analysis/fsen_fden_pelev_interaction.R
@@ -23,7 +23,11 @@ riskratio_data$dden <- dden_data$dden_adult
 abn <- with(spdata[spdata$sp %in% riskratio_data$sp,], tapply(sp, sp, length))
 riskratio_data$Abundance <- abn[!is.na(abn)]
 
-riskratio_data$fden <- cut(riskratio_data$dden, breaks = c(min(riskratio_data$dden)-.1, mean(riskratio_data$dden), max(riskratio_data$dden)), labels = c("min","max"))
+riskratio_data$fden <- cut(riskratio_data$dden, 
+                           breaks = c(min(riskratio_data$dden)-.1, 
+                                      mean(riskratio_data$dden), 
+                                      max(riskratio_data$dden)), 
+                           labels = c("low","high"))
 
 # Explore 
 ggplot(riskratio_data, aes(x = rr, y = pe)) + 
@@ -58,7 +62,7 @@ preds <- expand.grid(pe = with(riskratio_data,
                                seq(from = min(pe), 
                                    to = max(pe), 
                                    length = 100)),
-                     fden = c("min" , "max"))
+                     fden = c("low" , "high"))
 
 preds$p <- predict(model, preds, type = "response")
 preds$CI <- predict(object = model, 
@@ -68,16 +72,24 @@ preds$CI <- predict(object = model,
                     level = 0.95,
                     se.fit = TRUE)$se.fit
 
+# Colors
+col1 <- "#C5283D"
+col2 <-  "#255F85"
+
 # Graph the predictions 
 p1 <- ggplot(preds, aes(x = pe, y = p, fill = fden)) + 
-  geom_line() + 
-  geom_ribbon(aes(ymin = p - CI * 1.96, ymax = p + CI * 1.96), alpha = 0.22) +
+  geom_ribbon(aes(ymin = p - CI * 1.96, ymax = p + CI * 1.96), alpha = 0.5) +
   geom_point(data = riskratio_data, aes(x = pe, y = rr, color = fden)) + 
-  geom_line() + 
-  ylab("riskratio") + 
-  xlab("pelevation") +
+  ylab("flooding sensitivity") + 
+  geom_line(aes(color = fden), size = 1) + 
+  geom_line(size = 0.33) + 
+  geom_line(data = preds, aes(y = p - CI * 1.96, color = fden), linetype = 2, alpha = 0.222) + 
+  geom_line(data = preds, aes(y = p + CI * 1.96, color = fden), linetype = 2, alpha = 0.222) + 
+  xlab("pelevation (m)") +
   theme_classic() +
-  theme(legend.position = c(0.2, 0.85))
+  theme(legend.position = c(0.2, 0.85)) +
+  scale_fill_manual(values = c(col1, col2)) +
+  scale_color_manual(values = c(col1, col2))
 
 p1
 
@@ -86,21 +98,5 @@ ggsave(p1, file = './graphs/fsen_fden_pe1_interaction.png',
        width = 4, 
        height = 4)
 
-################################################################################
 
-
-##### bootstrap the data 
-preds <- expand.grid(pe = with(riskratio_data, 
-                               seq(from = min(pe), 
-                                   to = max(pe), 
-                                   length = 100)),
-                     fden = c("min" , "max"))
-
-preds$p <- predict(model, preds, type = "response")
-preds$CI <- predict(object = model, 
-                    newdata = preds, 
-                    type = "response", 
-                    interval = "confidence",
-                    level = 0.95,
-                    se.fit = TRUE)$se.fit
 
