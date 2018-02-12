44a73f1562502f488733be8c6538ca633d89c6b1
t03jam8 <james.margrove@usys.ethz.ch>
2018-02-12 14:21:09 +0100

    todays, work on 4ha plot analysis

--- analysis/Reuben_data/exploration and organisation.R
+++ analysis/Reuben_data/exploration and organisation.R
@@ -9,9 +9,18 @@ a3_data <- read.table("./data/Reuben_data/A3.txt", header = TRUE)
 s1_data <- read.table("./data/Reuben_data/S1.txt", header = TRUE)
 s2_data <- read.table("./data/Reuben_data/S2.txt", header = TRUE)
 s3_data <- read.table("./data/Reuben_data/S3.txt", header = TRUE)
-
-levels(a2_data$Species)
 # Filter species of interest
+head(a1_data)
+
+# Points with elevation 
+elev <- read.csv("./data//Reuben_data/pointsWithElevation.csv")
+
+elev$plot <- paste(elev$Forest, elev$id, sep = "")
+
+
+
+
+
 
 spp <- c("Dryobalanopslanceolata", "Parashoreamalaanonan", "Parashoreatomentella", 
          "Shorealeprosula", "Shoreamacroptera", "Shoreaparvifolia", 
@@ -22,23 +31,55 @@ spp <- c("Dryobalanopslanceolata", "Parashoreamalaanonan", "Parashoreatomentella
 
 
 data <- rbind(a1_data, a2_data, a3_data, s1_data, s2_data, s3_data)
+head(data)
 
-spn <- with(data[data$Species %in% spp,], tapply(Species, Species, length))
+spn <- with(data[data$Species %in% spp && data$Diam2000 > 50,], tapply(Species, Species, length))
+df <- with(data[data$Species %in% spp && data$Diam2000 > 50,], tapply(Species, list(Species, Forest), length))
+
+
+dt <- df[rownames(df) %in% spp,]
+
 df <- with(data[data$Species %in% spp,], tapply(Species, list(Species, Forest), length))
 
+str(df)
+
+with(elev, tapply(sepilok_DT, plot, mean))
 
 dt <- df[rownames(df) %in% spp,]
+n = as.vector(dt)
+#n[is.na(n)] <- 0
+sp = rep(rownames(dt)[order(rownames(dt))], 6)
+el <- rep(with(elev, tapply(sepilok_DT, plot, mean)), each = 14)
 
-df <- with(data[data$Species %in% spp,], tapply(Species, list(Species, ha4plot, Forest), length))
+dden <- read.table('./data/dden_adult.txt', header = T)
+dden <- dden[order(dden$sp),]
+dden <- dden[-c(4, 7),]
 
-df
-dt <- df[rownames(df) %in% spp,]
-str(a1_data)
+wd <- rep(dden$dden_adult, 6)
+
+rr_data <- read.table("./data/riskratio_data.txt", header = TRUE)[-c(4, 7), ]
+rr <- rep(rr_data$rr, 6)
+
+data <- data.frame(sp, n, el, wd, rr)
+
+data[!is.na(data$n),]
+
+m <- (lm(el ~ wd + rr, data[!is.na(data$n),]))
+car::vif(m)
 
 
+require(ggplot2)
+
+ggplot(data[!is.na(data$n),], aes(x = rr, y = el)) + geom_point(alpha = 0.1) + 
+  stat_smooth(method = lm)
+
+ggplot(data[!is.na(data$n),], aes(x = wd, y = el, color = rr, size = n*10)) + geom_point() + 
+  stat_smooth(method = lm) + 
+  theme_classic()
 
 
+data
 
-dt
+
 
 
