615fa5674fd9c699de10bbb1d2148022e41f2ffe
t03jam8 <james.margrove@usys.ethz.ch>
2018-03-19 17:15:35 +0100

    wden triangle

--- analysis/wood_density_triangle.R
+++ analysis/wood_density_triangle.R
@@ -1,4 +1,3 @@
-
 ######## author: James Margrove 
 
 # Set langauge and clear workspace
@@ -14,11 +13,10 @@ require(ape)
 require(McSpatial)
 # Import data 
 pelev_data <- read.table("./data/pelev_data.txt", header = TRUE)
-spatial_data <- read.table("./data/forestplot_160_spatial_data.txt", header = TRUE)
+spatial_data <- read.table("./data/forestplot_160_spatial_data_UPDATE.txt", header = TRUE)
 plotExtent <- read.table("./data/plotExtent.txt", header = TRUE)
-# Reubens data (Sort out later )
-#reu_data <- read.table( "./data/Reuben_data/data_cleaned.txt", header = TRUE)
-648/3
+reu_data <- read.table("./data/reu_160plot_combinded_data.txt", header = TRUE)
+
 # Cut the data to ensure that 
 Xbreaks <- seq(plotExtent[1,"X50N"], plotExtent[3,"X50N"], length = 37)  
 Ybreaks <- seq(plotExtent[1,"Y50N"], plotExtent[2,"Y50N"], length = 19)  
@@ -33,166 +31,58 @@ x <- as.vector(unlist(with(spatial_data, tapply(X50N, list(XCut, YCut), mean, na
 y <- as.vector(unlist(with(spatial_data, tapply(Y50N, list(XCut, YCut), mean, na.rm = TRUE))))
 
 
-
 spat_data <- data.frame(e, d, x, y)
 spat_data <- spat_data[!is.na(d),]
+ggplot(spat_data, aes(x = d, y = e)) + geom_point()
 
-o <- numeric()
-for(i in 1:length(spat_data$e)){
-  o[i] <- length(which(spat_data$e < spat_data$e[i]+5 & spat_data$e > spat_data$e[i]-5))
-}
+# Divide reubens plot data up in to 1/4 ha areas 
+with(reu_data, tapply(elev, list(Forest, ha4plot), mean))
 
-spat_data$o <- o
-ggplot(spat_data, aes(x = e, y = d, size = 1/o)) + geom_point()
-
-taus <- seq(0.025, 0.975, length = 5)
-
-mod <- rq(d ~ e, weight = 1/o, tau = taus, data = spat_data)
-mod
-summary(mod)
-
-str(mod)
-
-dt <- data.frame(taus = taus, coef = mod$coefficients[2,])
-
-ggplot(dt, aes(x = taus, y = (coef))) + geom_point() + geom_line() + 
-  geom_abline(intercept = 0, slope = 0, linetype = 2, color = "red") + 
-  theme_classic()
-
-
-quantile(spat_data$d, c(0.75))
-# accoring to this species > 0.5 wood density do not distribute themselves across elevation gradients
-# where as speceis below 0.5 are distributed across the elevatoin gradient?
-source("./functions/booter.R")
-ggplot(spat_data, aes(x = e, y = d, size = 1/o)) + geom_point()
-pred1 <- data.frame(e = seq(min(e, na.rm = T), max(e, na.rm = T), length = 100))
-prd <- expand.grid(e = seq(min(e, na.rm = T), max(e, na.rm = T), length = 100), taus = taus)
-prd$d <- as.vector(predict(mod, pred1))
-args(booter)
-CI <- booter(model = mod, data = spat_data, preds = pred1, quantReg = TRUE, n = 1000)
-prd$CI025 <- CI[1,]
-prd$CI975 <- CI[2,]
-
-
-# Colors for CI ribbon 
-cols <- c("#8CB369", "#F4E285", "#4C8577","#F4A259", "#BC4B51")
-
-p1 <- ggplot(spat_data, aes(x = e, y = d, size = 1/o)) + geom_point() + 
-  geom_ribbon(data = prd, inherit.aes = F, aes(x = e, ymin = CI025, ymax = CI975, group = taus, fill = factor(taus)), alpha = 0.75) + 
-  ylim(0.3, 0.65) + 
-  theme_classic() + 
-  geom_line(data = prd, inherit.aes = F, aes(x = e, y = d, group = taus)) + 
-  scale_fill_manual(values = cols) + 
-  ylab(bquote("Wood density g" ~cm^-3 )) +
-  xlab("elevation (asl m)") + 
-  scale_color_manual(values = cols) + 
-  theme(legend.position = "top")
-  
-p1
-
-
-
-head(prd)
-
-require(ade4)
-coords_dist <- dist(cbind(spat_data$x, spat_data$y))
-dden_dist <- dist(spat_data$d)
-#mantel.rtest(coords_dist, dden_dist, nrepet = 9999)
-
-# Are wood density values auto-correlated?
-# Calculate Morans I
-coords_dist_mat <- as.matrix(dist(cbind(spat_data$x, spat_data$y)))
-coords_dist_mat_inv <- 1/coords_dist_mat
-diag(coords_dist_mat_inv) <- 0
-# Calculate Moran's I
-Moran.I(spat_data$d, coords_dist_mat_inv)
-# full data set is very autocorrelated, this is a major issue. And
-
-# The Moran and Mantel tests show that there is spatial autocorrelation in the values. 
-# We need to account for this by doing a quantreg accounting for this 
-
-taus <- c(0.025, 0.1, 0.5, 0.9, 0.975)
-
-linerEQ <- function(int, slope, x){
-  (x*slope) + int
-}
-
-n = as.integer(dim(spat_data)[1]/3)
-
-qrSpatial <- function(data){
-  rown <- sample(1:dim(data)[1], replace = T, size = n)
-  samp <- data[rown,]
-  coords_dist <- dist(cbind(samp$x, samp$y))
-  dden_dist <- dist(samp$d)
-  mt <- mantel.rtest(coords_dist, dden_dist, nrepet = 999)
-  if(mt$pvalue > 0.05){
-    qrmod <- rq(e ~ d, data = samp, tau = taus)
-    p <- predict(qrmod, data.frame(d = seq(min(data$d), max(data$d), length = 100)))
-    return(as.vector(p))
+for(i in levels(reu_data$Forest)){
+  for(j in as.character(1:4)){
+    subplot <- reu_data[reu_data$Forest == i & reu_data$ha4plot == j,]
+    
+    xr <- range(subplot[, "x"])
+    yr <- range(subplot[, "y"])
+    
+    midPoint <- c(mean(range(subplot[, "x"])), mean(range(subplot[, "y"])))
+    
+    quarter1 <- subplot[subplot$x > xr[1] & subplot$x < midPoint[1] & 
+                          subplot$y > yr[1] & subplot$y < midPoint[2],]
+    
+    quarter2 <- subplot[subplot$x > xr[1] & subplot$x < midPoint[1] & 
+                          subplot$y < yr[2] & subplot$y > midPoint[2],]
+    
+    quarter3 <- subplot[subplot$x < xr[2] & subplot$x > midPoint[1] & 
+                          subplot$y < yr[2] & subplot$y > midPoint[2],]
+    
+    quarter4 <- subplot[subplot$x < xr[2] & subplot$x > midPoint[1] & 
+                          subplot$y > yr[1] & subplot$y < midPoint[2],]
+    
+    quarter1$Q <- rep("Q1", nrow(quarter1))
+    quarter2$Q <- rep("Q2", nrow(quarter2))
+    quarter3$Q <- rep("Q3", nrow(quarter3))
+    quarter4$Q <- rep("Q4", nrow(quarter4))
+    
+    
+    quaters <- rbind(quarter1, quarter2, quarter3, quarter4)
+    
+    new_row <- data.frame(e = with(quaters, tapply(elev, Q, mean)),
+                          d = with(quaters, tapply(dden, Q, mean)), 
+                          x = with(quaters, tapply(x, Q, mean)),
+                          y = with(quaters, tapply(y, Q, mean)))
+    
+    spat_data <- rbind(spat_data, new_row)
   }
 }
 
-# Boot strap without the data
-#dist <- foreach(i = 1:5000, .combine = cbind) %do% qrSpatial(data = spat_data)
 
-# save the boot strap 
-#write.table(dist, file = "./data/boots_triangle_wden.txt")
-dist <- read.table("./data/boots_triangle_wden.txt", header = TRUE)
+# how many species are there in total 
 
-## how many were successsfully independent 
-mdist <- apply(dist, 1, mean)
+nuSp <- (unique(c(levels(spatial_data[!is.na(spatial_data$dden),]$fullname), 
+                  levels(reu_data[!is.na(reu_data$dden),]$Species))))
+# how many individuals in total 
 
-# prediction data frame 
-preds <- expand.grid(d = seq(min(spat_data$d), max(spat_data$d), length = 100), taus = as.factor(taus))
-preds$e <- apply(dist, 1, mean)
-CI <- apply(dist, 1, quantile, c(0.025, 0.975))
-preds$CI025 <- CI[1,]
-preds$CI975 <- CI[2,]
+nrow(reu_data) + nrow(spatial_data)
 
-# Colors for CI ribbon 
-cols <- c("#8CB369", "#F4E285", "#4C8577","#F4A259", "#BC4B51")
-
-# plot the results 
-p1 <- ggplot(preds[preds$d < 0.6,], aes(x = d, y = e, group = taus)) + 
-  geom_ribbon(aes(ymin = CI025, ymax = CI975, group = taus, fill = taus), alpha = 0.75) + 
-  geom_point(data = spat_data[spat_data$d < 0.6, ], inherit.aes = FALSE, aes(x = d, y = e), alpha = 0.4) + 
-  theme_classic() + 
-  geom_line() + 
-  xlab(bquote("Wood density g" ~cm^-3 )) +
-  ylab("elevation (asl m)") + 
-  scale_fill_manual(values = cols) + 
-  theme(legend.position = "top")
-
-
-p1
-
-# Save the graph 
-ggsave(p1, file = './graphs/wden_triangle.png', 
-       width = 4, 
-       height = 4.2)
-
-
-
-ribbon_data <- data.frame(d = seq(min(spat_data$d), max(spat_data$d), length = 100))
-
-ribbon_data$CI025 <- preds[preds$taus == "0.025",]$CI025
-ribbon_data$CI975 <- preds[preds$taus == "0.975",]$CI975
-
-
-p2 <- ggplot(preds[preds$d < 0.6,], aes(x = d, y = e, group = taus)) + 
-  geom_ribbon(data = ribbon_data[ribbon_data$d < 0.6,], inherit.aes = F, aes(x = d, ymin = CI025, ymax = CI975), fill = "blue",alpha = 0.2) + 
-  geom_point(data = spat_data[spat_data$d < 0.6, ], inherit.aes = FALSE, aes(x = d, y = e), alpha = 0.4) + 
-  theme_classic() + 
-  geom_line(aes(color = taus)) + 
-  xlab(bquote("Wood density g" ~cm^-3 )) +
-  ylab("elevation (asl m)") + 
-  scale_color_manual(values = cols) + 
-  theme(legend.position = "top")
-
-
-p2
-
-ggsave(p2, file = './graphs/wden_triangle_constant_envalope.png', 
-       width = 4, 
-       height = 4.2)
-
+write.table(spat_data, file = "./data/wden_triangle_data.txt")
