f715db6661d7d1f0dc9bceeeb13e42f2e0b96cb9
t03jam8 <james.margrove@usys.ethz.ch>
2018-03-19 17:14:36 +0100

    added files

--- analysis/Prediction_specieslevel.R
+++ analysis/Prediction_specieslevel.R
@@ -13,28 +13,28 @@ source("./functions/booter.R")
 # Import data 
 sp <- read.table("./data/riskratio_data.txt", header = TRUE)$sp[-c(4,7, 13)]
 data <- read.table( "./data/Reuben_data/data_cleaned.txt", header = TRUE)
+with(data, tapply(dden, Species, mean))
+# Import model 
 load("./models/pele_fsen_dden_Abundance")
 summary(model)
 # Predict elevation
 data$p <- predict(model, data, type = "response")
-data$sd <- predict(model, data, type = "response", se.fit = TRUE)$se.fit
-
 head(data)
 
 # organsie data in to a data.frame
 e <- as.vector(with(data, tapply(elev, Species, mean, na.rm = T)))
 p <- as.vector(with(data, tapply(p, Species, mean)))
 a <- (as.vector(with(data, tapply(p, Species, length))))
-dt <- data.frame(e, p, a, sp)
-dt
-
+dden <- round(as.vector(with(data, tapply(dden, Species, mean))), 2)
+dt <- data.frame(e, p, a, sp, dden)
 rownames(dt) <- c()
 
 # SD of the predictions 
-preds <- data.frame(rr = as.vector(with(data, tapply(rr, Species, mean))), dden = as.vector(with(data, tapply(dden, Species, mean))))
+preds <- data.frame(rr = as.vector(with(data, tapply(rr, Species, mean))), 
+                    dden = as.vector(with(data, tapply(dden, Species, mean))))
 
 # Run native model 
-model3 <- lm(e ~ p, weights = a, dt)
+model3 <- lm(e ~ p, weight = a, dt)
 summary(model3)
 
 # 95% coef
@@ -43,53 +43,30 @@ coef[2,2]*1.96*c(1,-1) + coef[2,1]
 coef[1,2]*1.96*c(1,-1) + coef[1,1]
 
 # Calculate the R^2 manually, for all individuals within the plots 
-indvPred <- predict(model, data, type = "response")
-SSE_H1 <- sum((data$el - indvPred)^2)
-SSE_H0 <- sum(((data$el) - mean(data$el))^2)
-R2 <- (1 - SSE_H1/SSE_H0)
+SSE_H1 <- sum((data$elev - predict(model3, data, type = "response"))^2)
+SSE_H0 <- sum((data$elev - mean(data$elev))^2)
+R2 <- (1 - (SSE_H1/SSE_H0))
 R2 
+
+resid(lm(elev ~ p, data))[1:10]
+(data$elev - data$p)[1:10]
 
 # nudge text 
 dt$nudge_text <- rep(0.5, 13)
 dt$nudge_text[which(dt$sp == "Slep")] <- -1
 dt$nudge_text[which(dt$sp == "Spar")] <- -1
+dt$nudge_text[which(dt$sp == "Ptom")] <- 2
+dt$nudge_text[which(dt$sp == "Smec")] <- -1
+dt$nudge_text[which(dt$sp == "Swal")] <- -1
+dt$nudge_text[which(dt$sp == "Sfal")] <- -1
 
 preds <- data.frame(p = seq(min(p),max(p), length = 100))
 preds$e <- predict(model3, preds, type = "response")
 preds$CI <- predict(model3, preds, type = "response", se.fit = TRUE)$se.fit * 1.96
 # jitter values 
-j <- rep(c(130, 125, 130), 7)[1:13] + 5
+j <- rep(c(130, 125), 7)[1:13] + 10
 
 colnames(dt)[3] <- "Abundance"
-
-p2 <- ggplot(dt, aes(x = p, y = e)) + 
-  geom_violin(data = data, aes(x = round(p, 2), y = elev, group = Species),  scale = "count", width = 15) +
-  geom_point(aes(size = Abundance), alpha = 0.33) +
-  geom_ribbon(data = preds, aes(ymin = e - CI, ymax = e + CI), alpha = 0.2) + 
-  geom_line(data = preds, aes(y = e + CI, x = p), alpha = 0.2) + 
-  geom_line(data = preds, aes(y = e - CI, x = p), alpha = 0.2) + 
-  geom_line(data = preds, aes(x = p, y = e)) +
-  theme_classic() + 
-  geom_text(aes(x = p, y = j, label = sp, size = 20, angle = 60), nudge_x = dt$nudge_text, show.legend = FALSE) + 
-  geom_point(aes(x = p, y = j), size = 0.2) + 
-  geom_vline(xintercept = p, alpha = 0.2, linetype = 2) + 
-  theme(legend.position="top") +
-  xlab("Predicted species elevation (asl m)") +
-  ylab("Actual species median elevation (asl m)") + 
-  geom_abline(intercept = 0, slope = 1, linetype = 2, color = "#BC4B51") + 
-  geom_hline(yintercept = 67.43401, linetype = 2, alpha = 0.2) 
-
-p2
-
-
-
-
-
-ggsave(p2, file = './graphs/Reuben_meanPlotElevation_Violin.png', 
-       width = 4, 
-       height = 4.2)
-
-
 
 p2 <- ggplot(dt, aes(x = p, y = e)) + 
   geom_vline(xintercept = p, alpha = 0.2, linetype = 2) +
@@ -104,13 +81,14 @@ p2 <- ggplot(dt, aes(x = p, y = e)) +
   geom_point(aes(x = p, y = j), size = 0.2) + 
   theme(legend.position="top") +
   xlab("Predicted species elevation (asl m)") +
-  ylab("Actual species median elevation (asl m)") + 
+  ylab("Actual species mean elevation (asl m)") + 
   geom_abline(intercept = 0, slope = 1, linetype = 2, color = "#BC4B51") + 
   geom_hline(yintercept = 67.43401, linetype = 2, alpha = 0.2) 
 
 p2
 
-ggsave(p2, file = './graphs/Reuben_medianPlotElevation.png', 
+
+ggsave(p2, file = './graphs/Reuben_meanPlotElevation.png', 
        width = 4, 
        height = 4.2)
 
--- analysis/floodingsensitivity_speciesdistributions.R
+++ analysis/floodingsensitivity_speciesdistributions.R
@@ -13,40 +13,33 @@ source("./functions/aovPerVar.R")
 source("./functions/booter.R")
 # IMPORT DATA 
 riskratio_data <- read.table("./data/riskratio_data.txt", header = TRUE)
-spdata <- read.table("./data/forestplot_160_spatial_data.txt", header = TRUE)
+spdata <- read.table("./data/forestplot_160_spatial_data_UPDATE.txt", header = TRUE)
 riskratio_data$pe <- read.table("./data/pelev_data.txt", header = TRUE)$pe
 
 # Calculating the abundance 
 abn <- with(spdata[spdata$sp %in% riskratio_data$sp,], tapply(sp, sp, length))
 riskratio_data$Abundance <- abn[!is.na(abn)]
-dden_data <- read.table("./data/dden_adult.txt", header = TRUE)
+dden_data <- read.table("./data/dden_adult_new.txt", header = TRUE)
 
 dden_data <- dden_data[order(dden_data$sp), ]
 riskratio_data$dden <- dden_data$dden_adult
 
-# Explore 
-ggplot(riskratio_data, aes(x = rr, y = pe, size = Abundance/160)) + 
-  geom_point() + 
-  stat_smooth(method = lm)
-
-# How much just just the low wood density group r
-riskratio_data$fden <- cut(riskratio_data$dden, 
-                    breaks = c(min(riskratio_data$dden)-.1, 
-                               mean(riskratio_data$dden), 
-                               max(riskratio_data$dden)), 
-                    labels = c("low","high"))
-
-
-with(riskratio_data[riskratio_data$fden == "low",], cor(rr, pe))
-
 # Model data with a weighted linear model 
-model <- lm(pe ~ rr + dden, weights = Abundance, data = riskratio_data)
+model <- lm(pe ~ rr + dden, data = riskratio_data)
 summary(model)
+av1 <- anova(model)
+av1
+av1[,2]/sum(av1[,2]) *100
+
+car::Anova(model)
+aovPerVar(model)
+
 
 vif(model)
+
 save(model, file = "./models/pele_fsen_dden_Abundance")
 
-#booter(model, data = riskratio_data, coef = TRUE, n = 5000)
+booter(model, data = riskratio_data, coef = TRUE, n = 5000)
 
 # Runing the univariate model for the line
 uniMod_rr <- (lm(pe ~ rr, weights = Abundance, data = riskratio_data))
@@ -75,21 +68,21 @@ preds$CI <- predict(model, preds, type = "response", se.fit = TRUE)$se.fit
 
 # Graph the predictions 
 p1 <- ggplot(preds, aes(x = rr, y = p)) + 
-        geom_line() + 
-        geom_ribbon(aes(ymin = p - CI * 1.96, ymax = p + CI * 1.96), alpha = 0.22) +
-        geom_line(aes(x = rr, y = p-CI * 1.96), linetype = 2, alpha = 0.1) + 
-        geom_line(aes(x = rr, y = p+CI * 1.96), linetype = 2, alpha = 0.1) + 
-        geom_point(data = riskratio_data, aes(x = rr, y = pe, size = Abundance), alpha = 0.3) + 
-        geom_line() + 
-        ylab("p(elevation) m asl") + 
-        xlab("Water inundation sensitivity") +
-        theme_classic() +
-        theme(legend.position = "none") +
+  geom_line() + 
+  geom_ribbon(aes(ymin = p - CI * 1.96, ymax = p + CI * 1.96), alpha = 0.22) +
+  geom_line(aes(x = rr, y = p-CI * 1.96), linetype = 2, alpha = 0.1) + 
+  geom_line(aes(x = rr, y = p+CI * 1.96), linetype = 2, alpha = 0.1) + 
+  geom_point(data = riskratio_data, aes(x = rr, y = pe, size = Abundance), alpha = 0.3) + 
+  geom_line() + 
+  ylab("p(elevation) m asl") + 
+  xlab("Water inundation sensitivity") +
+  theme_classic() +
+  theme(legend.position = "none") +
   geom_text(aes(x = 0.12, y = 110, 
                 label = paste("ANOVA: ", round(aov_percent[1], 1), "%", sep = "")), size = 3) +
   geom_abline(intercept = coef(uniMod_rr)[1], slope = coef(uniMod_rr)[2], linetype = 2, color = "#BC4B51")
 
-  p1
+p1
 
 # Save plot to graphs file 
 ggsave(p1, file = './graphs/pelevation_fsen_Abundance.png', 
@@ -99,9 +92,9 @@ ggsave(p1, file = './graphs/pelevation_fsen_Abundance.png',
 ################################################################################
 # bootstrap the data 
 preds <- expand.grid(dden = with(riskratio_data, 
-                               seq(from = min(dden), 
-                                   to = max(dden), 
-                                   length = 100)), 
+                                 seq(from = min(dden), 
+                                     to = max(dden), 
+                                     length = 100)), 
                      rr = mean(riskratio_data$rr))
 
 preds$p <- predict(model, preds, type = "response")
@@ -120,7 +113,7 @@ p2 <- ggplot(preds, aes(x = dden, y = p)) +
   xlab(bquote("Wood density g" ~cm^-3 )) +
   theme_classic() + theme(legend.position = "none") + 
   theme(legend.position = "none") +
-  geom_text(aes(x = 0.35, y = 120, 
+  geom_text(aes(x = 0.4, y = 120, 
                 label = paste("ANOVA: ", round(aov_percent[2], 1), "%", sep = "")), size = 3) +
   geom_abline(intercept =  coef(uniMod_dd)[1], slope = coef(uniMod_dd)[2], linetype = 2, color = "#BC4B51")
 
