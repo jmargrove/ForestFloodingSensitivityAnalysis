ac154918da573de8156ebb942d9cc8ec2cef5376
t03jam8 <grovejam@gmail.com>
2018-10-23 14:12:33 +0200

    Functions added

--- functions/pointDist.R
+++ functions/pointDist.R
@@ -8,9 +8,9 @@
 #' @param p1 coordinates of point 1
 #' @param p2 coordinates of point 2
 
-pointDist <- function(p1,p2){
-  xvals <- (p2[1]-p1[1])^2
-  yvals <- (p2[2]-p1[2])^2
-  sumVals = xvals+yvals
-  sqrt(sumVals)
+pointDist <- function(p1, p2){
+  xvals <- (p2[1] - p1[1]) ^ 2
+  yvals <- (p2[2] - p1[2]) ^ 2
+  sum_vals <- xvals + yvals
+  sqrt(sum_vals)
 }
--- functions/quaterPoly.R
+++ functions/quaterPoly.R
@@ -1,10 +1,7 @@
 ################################################################################
 #' @title quaterPoly
-#' 
 #' @author James Margrove 
-#'
 #' @description funciton that takes coordinates and partions plots 
-#'
 #' @param coords four sets of coordinates, 
 #' @param graph outpust a ggplot2 graph of the 4 new polygons 
 
@@ -13,14 +10,14 @@ quaterPoly <- function(coords, graph = F){
   source("./functions/midPoint.R")
   source("./functions/sortToPoly.R")
   newcoords <- data.frame(x = as.numeric(), y = as.numeric())
-  coords <- sortToPoly(coords)
+  coords <- sort_to_poly(coords)
   s = 1:4
   for(i in s){
     newdt <- coords[i,]
     for(j in s[-i]) {
       newdt <- rbind(newdt, midPoint(coords[i,], coords[j, ]))
     }
-    newdt <- sortToPoly(newdt)
+    newdt <- sort_to_poly(newdt)
     newcoords <- rbind(newcoords, newdt)
   }
   newcoords$Q <- factor(rep(1:4, each = 4))
