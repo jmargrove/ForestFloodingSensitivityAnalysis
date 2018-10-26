ad6eeac505934536efaafd0b74a6f69a88180ba1
t03jam8 <grovejam@gmail.com>
2018-10-26 13:48:26 +0200

    Path was wrong!

--- analysis/adult_distribution_analysis/analysis/adult_distribution_INLA_SPDE_remote.R
+++ analysis/adult_distribution_analysis/analysis/adult_distribution_INLA_SPDE_remote.R
@@ -1,7 +1,7 @@
 # adult distribution INLA SPDE analysis
 rm(list = ls())
 print(paste('working dir:::', getwd()))
-path <- '/home/majames/Documents/ForestFloodingSensitivityAnalysis/adult_distribution_analysis/'
+path <- '/home/majames/Documents/ForestFloodingSensitivityAnalysis/analysis/adult_distribution_analysis/'
 #pathe <- './'
 
 source(paste(path, 'data/data_index.R', sep = ""))
