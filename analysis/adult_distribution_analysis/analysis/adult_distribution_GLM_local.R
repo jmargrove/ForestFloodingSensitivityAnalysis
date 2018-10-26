87b3840387c4ec4672201d0a8740033b5ce7d16f
t03jam8 <grovejam@gmail.com>
2018-10-26 13:33:50 +0200

    Comments in code

--- analysis/adult_distribution_analysis/analysis/adult_distribution_GLM_local.R
+++ analysis/adult_distribution_analysis/analysis/adult_distribution_GLM_local.R
@@ -2,16 +2,18 @@
 source('./analysis/adult_distribution_analysis/data/data_index.R')
 str(species_occurance_data)
 
-# very quick model of the data 
+#Modelling occurance data as a binomial glm 
 model1 <- glm(occurance ~ species * elev + I(elev^2), family = 'binomial', data = species_occurance_data)
 summary(model1)
 
+#Creating prediction data frame 
 preds <- expand.grid(species = levels(species_occurance_data$species), 
                      elev = with(species_occurance_data, seq(min(elev), max(elev), length = 100)))
-
+# Predicting values 
 preds$occurance <- predict(model1, preds, type = 'response')
 preds$CI025 <- preds$occurance + predict(model1, preds, type = 'response', se.fit = TRUE)$se.fit * -1.96
 preds$CI975 <- preds$occurance + predict(model1, preds, type = 'response', se.fit = TRUE)$se.fit * 1.96
+
 
 require(ggplot2)
 # graph of the prediced values...
