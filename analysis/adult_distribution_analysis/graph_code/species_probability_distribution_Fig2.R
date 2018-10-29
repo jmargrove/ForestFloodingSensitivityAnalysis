acac8fe162753c92d8a51fa7ee76f47a9e1ceaa9
t03jam8 <grovejam@gmail.com>
2018-10-29 14:23:26 +0100

    Species distribution graph sorted

--- analysis/adult_distribution_analysis/graph_code/species_probability_distribution_Fig2.R
+++ analysis/adult_distribution_analysis/graph_code/species_probability_distribution_Fig2.R
@@ -1,4 +1,5 @@
 # graphing the species probability distribution 
+rm(list = ls())
 
 Sys.setenv(LANG = "en")
 
@@ -7,40 +8,38 @@ require(ggplot2)
 require(gtable)
 require(grid)
 
-# Import data 
-sp_dist_pred <- read.table("./data/species_dist_predictions.txt", header = TRUE)
 
-#elevation_bootstrap <- read.table("./data/elevation_bootstrap.txt", header = TRUE)
+# Import data 
+load('./analysis/adult_distribution_analysis/data/elevation_distribution_CI_NEW.R')
+pelev_data <- read.table("./SRF_spatial_analysis_elevation/SRF_spatial_analysis_elevation/pelev_data.txt", header = TRUE)
+spatial_data <- read.table("./SRF_spatial_analysis_elevation/SRF_spatial_analysis_elevation/forestplot_160_spatial_data.txt", header = TRUE)
+spdata <- droplevels(spatial_data[spatial_data$sp %in% levels(pelev_data$sp),])
+sp_dist_pred <- read.table("./SRF_spatial_analysis_elevation/SRF_spatial_analysis_elevation/preds_curves.txt", header = TRUE)
 spnames <- read.table("./data/species_names.txt", header = TRUE)
 
-pelev_data <- read.table("./data/pelev_data.txt", header = TRUE)
-spatial_data <- read.table("./data/forestplot_160_spatial_data.txt", header = TRUE)
-spdata <- droplevels(spatial_data[spatial_data$sp %in% levels(pelev_data$sp),])
-CI <- apply(elevation_bootstrap, 1, quantile, c(0.025, 0.975))
-quantile(elevation_bootstrap[70,], c(0.025, 0.975))
 
 
-write.table(CI, file = "analysis/adult_distribution_analysis/data/elevation_distribution_CI.txt")
-elevation_CI <- read.table("analysis/adult_distribution_analysis/data/elevation_distribution_CI.txt", header = TRUE)
+dim(sp_dist_pred)/16
+head(sp_dist_pred)
 
+dim(CI)/16
 # Preperation of the main data frame of the curves
-sp_dist_pred$spname <- with(spnames[spnames$sp %in% levels(pelev_data$sp),], rep(paste(Cap,species), each = 121))
+sp_dist_pred$spname <- with(spnames[spnames$sp %in% levels(pelev_data$sp),], rep(paste(Cap,species), each = 131))
 sp_dist_pred$spname <- factor(sp_dist_pred$spname)
 sp.levels <- levels(reorder(levels(sp_dist_pred$spname), pelev_data$pe))
 sp_dist_pred$spname <- factor(sp_dist_pred$spname, levels=sp.levels)
 
 # Adding confidence intervals to the data frame 
-CIwhere <- c(rep(c(rep(TRUE, 121), rep(FALSE, 10)), 16), rep(FALSE, 16))
-sp_dist_pred$CI025 <- CI[1,CIwhere]
-sp_dist_pred$CI975 <- CI[2,CIwhere]
+sp_dist_pred$CI025 <- CI[1, 1:(131*16)]
+sp_dist_pred$CI975 <- CI[2, 1:(131*16)]
 
-
+dim(sp_dist_pred)/16
+dim(CI)/16
 # Graph 
 p1 <- ggplot(sp_dist_pred[sp_dist_pred$elev > min(spdata$elev) & sp_dist_pred$elev < max(spdata$elev),], aes(x = elev, y = p)) + 
   geom_ribbon(aes(ymin=CI025,ymax=CI975,linetype=NA), alpha =  0.22) + 
   geom_line() + 
   facet_wrap(~spname, scale="free_y") + 
-  # xlim(min(spdata$elev),max(spdata$elev)) + 
   theme_classic() + 
   theme(axis.line.x=element_line(color="black"), axis.line.y=element_line(color="black")) + 
   xlab("Elevation (m)") + ylab("p(occurance)") + 
