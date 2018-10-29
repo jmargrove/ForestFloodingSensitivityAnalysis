a401f03d1ef7e6dca95ef2c79d8ecaa00d87256e
t03jam8 <grovejam@gmail.com>
2018-10-29 23:49:27 +0100

    riskratio prediction

--- analysis/inundation_predicts_species_distributions/graph_code/riskratio_prediction_Fig3.R
+++ analysis/inundation_predicts_species_distributions/graph_code/riskratio_prediction_Fig3.R
@@ -25,7 +25,10 @@ partial_lines_data <- data.frame(x = rep(riskratio$diff_mort, 2),
                                  y = c(riskratio$elev, riskratio$partials_rr))
 # ANOVA type II to compare the variance 
 var_res <- car::Anova(model2)[,1]
-rr_explaied_variation <- paste('Anova:', round(var_res / sum(var_res) * 100, 1)[2], "%")
+rr_explaied_variation <- paste('ANOVA:', round(var_res / sum(var_res) * 100, 1)[2], "%")
+
+# jittering the species names 
+
 
 # plotting the data 
 p1 <- ggplot(preds_riskratio, aes(x = diff_mort, y = elev)) + 
@@ -34,18 +37,23 @@ p1 <- ggplot(preds_riskratio, aes(x = diff_mort, y = elev)) +
   geom_point(data = riskratio, aes(y = elev, x = diff_mort), alpha = 0.5, color = 'red') + 
   geom_ribbon(aes(ymin = CI025, ymax = CI975), alpha = 0.2) + 
   theme_bw() + 
-  geom_point(data = riskratio, aes(y = partials_rr, x = diff_mort), color = 'black') + 
-  geom_text(aes(x = 0.1, y = 125, label = rr_explaied_variation)) + 
+  geom_point(data = riskratio, aes(y = partials_rr, x = diff_mort), 
+             color = 'black', 
+             pch = 21, 
+             fill = 'grey', size = 3) + 
+  geom_text(aes(x = 0.12, y = 120, label = rr_explaied_variation), size = 5) + 
   ylab('E(elevation) m asl') + 
   xlab('Water inundation sensitivity') + 
-  ylim(25, 125) + 
+  ylim(40, 120) + 
   stat_smooth(data = riskratio, aes(x = diff_mort, y = elev), 
               se = F, method = 'lm', color = 'red', 
-              linetype = 2, size = 0.25)
+              linetype = 2, size = 0.25) + 
+  theme(text = element_text(size = 20)) + 
+  geom_text(data = riskratio, aes(label = sp), nudge_y = rep(2.3, 16), nudge_x = rep(0.01, 15))
 
 p1
 
 ggsave(p1, 
        file = './analysis/inundation_predicts_species_distributions/graph_code/graphs/elevation_VS_riskration_C_woodensity.png', 
-       width = 4, 
-       height = 4)
+       width = 6, 
+       height = 6)
