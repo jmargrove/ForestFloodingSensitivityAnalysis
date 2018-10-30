460b4d3d93bd805ff52d0652128cc3ef38187372
t03jam8 <grovejam@gmail.com>
2018-10-30 16:33:10 +0100

    Graph of multivariate analysis of wood density

--- analysis/inundation_predicts_species_distributions/graph_code/wooddensity_prediction_Fig3.R
+++ analysis/inundation_predicts_species_distributions/graph_code/wooddensity_prediction_Fig3.R
@@ -30,27 +30,58 @@ partial_lines_data <- data.frame(x = rep(riskratio$dden, 2),
 var_res <- car::Anova(model2)[,1]
 dd_explaied_variation <- paste('ANOVA:', round(var_res / sum(var_res) * 100, 1)[1], "%")
 
+# jittering the species names 
+vj <- rep(2.5, 16)
+vj[which(riskratio$sp == 'Spar')] <- -5
+vj[which(riskratio$sp == 'Smec')] <- -7
+vj[which(riskratio$sp == 'Sfal')] <- 5
+vj[which(riskratio$sp == 'Sxan')] <- 22
+vj[which(riskratio$sp == 'Swal')] <- -5
+
+#horazontal 
+hj <- rep(0.01, 16)
+hj[which(riskratio$sp == 'Spar')] <- -0.025
+hj[which(riskratio$sp == 'Smec')] <- -0.02
+hj[which(riskratio$sp == 'Slep')] <- 0.02
+hj[which(riskratio$sp == 'Pmal')] <- -0.02
+hj[which(riskratio$sp == 'Sacu')] <- -0.02
+hj[which(riskratio$sp == 'Sgib')] <- -0.02
+hj[which(riskratio$sp == 'Sbec')] <- 0.02
+hj[which(riskratio$sp == 'Spau')] <- 0.02
+hj[which(riskratio$sp == 'Sfal')] <- -0.02
+hj[which(riskratio$sp == 'Dry')] <- 0.02
+hj[which(riskratio$sp == 'Ssem')] <- -0.025
+hj[which(riskratio$sp == 'Ptom')] <- -0.025
+
 # plotting the data 
-p1 <- ggplot(preds_wooddensity, aes(x = dden, y = elev)) + 
+p1_wooddensity <- ggplot(preds_wooddensity, aes(x = dden, y = elev)) + 
   geom_line(data = partial_lines_data, aes(x = x, y = y, group = factor(x)), alpha = 0.3) + 
   geom_line() + 
   geom_ribbon(aes(ymin = CI025, ymax = CI975), alpha = 0.2) + 
-  geom_point(data = riskratio, aes(y = partials_dd, x = dden), color = 'black') +
+  geom_point(data = riskratio, aes(y = partials_dd, x = dden),
+             color = 'black', 
+             pch = 21, 
+             fill = 'grey', size = 3)  +
   theme_bw() +
   geom_point(data = riskratio, aes(y = elev, x = dden), color = 'red', alpha = 0.5) + 
-  geom_text(aes(x = 0.4, y = 125, label = dd_explaied_variation)) + 
+  geom_text(aes(x = 0.4, y = 120, label = dd_explaied_variation), size = 5) + 
   ylab('E(elevation) m asl') + 
-  xlab('Wood density') + 
-  ylim(25, 125) + 
+  xlab(expression("Wood density"~g~cm^-3)) + 
+  ylim(40, 120) + 
   stat_smooth(data = riskratio, aes(x = dden, y = elev), 
               se = F, method = 'lm', color = 'red', 
-              linetype = 2, size = 0.25)
+              linetype = 2, size = 0.5) + 
+  theme(text = element_text(size = 20)) + 
+  geom_text(data = riskratio, aes(label = sp), 
+            size = 5,
+            nudge_y = vj, 
+            nudge_x = hj, 
+            fontface = 'italic')
 
-p1
 
-ggsave(p1, 
-       file = './analysis/inundation_predicts_species_distributions/graph_code/graphs/elevation_VS_wooddensity_C_inundation.png', 
-       width = 4, 
-       height = 4)
-
+# save(p1_wooddensity, file = './analysis/inundation_predicts_species_distributions/graph_code/graphs/elevation_VS_wooddensity.R')
+# ggsave(p1, 
+#        file = './analysis/inundation_predicts_species_distributions/graph_code/graphs/elevation_VS_wooddensity_C_inundation.png', 
+#        width = 6, 
+#        height = 6)
 
